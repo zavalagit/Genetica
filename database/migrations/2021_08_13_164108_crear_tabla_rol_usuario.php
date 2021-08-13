e19f08e0b41fe656e6fb4968cc18be1d52c622e6
proyectogenetica <zavala@example.com>
2021-08-13 13:31:10 -0500

    creacion de tablas de permisos,roles,usuarios en migraciones

---
+++ database/migrations/2021_08_13_164108_crear_tabla_rol_usuario.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CrearTablaRolUsuario extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rol_usuario', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('rol_id')->unsigned();
+            $table->foreign('rol_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->integer('usuario_id')->unsigned();
+            $table->foreign('usuario_id')->references('id')->on('usuarios')->onDelete('cascade');
+            $table->boolean('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rol_usuario');
+    }
+}
