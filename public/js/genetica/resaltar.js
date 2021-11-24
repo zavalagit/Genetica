3fd8698c2ab232546c2810d51747cac24999bdfc
proyectogenetica <zavala@example.com>
2021-11-24 15:13:37 -0600

    resaltar parte2

--- public/js/genetica/resaltar.js
+++ public/js/genetica/resaltar.js
@@ -1,54 +1,25 @@
-
-
 $( '.resaltar' ).click(function() {
     $( this ).toggleClass( "borderrosa" );
     var title = $( this ).attr( "id" );
     var valor = $( this ).attr( "value" );
     var td_analizar = document.querySelectorAll('td[id="'+`${title}`+'"]');
-    text = td_analizar[0].innerHTML;
-    marked = text.replace(valor,"<mark>"+valor+"</mark>");
-    td_analizar[0].innerHTML = marked;
-    console.log(valor);
+    //console.log(td_analizar.length);
+    for (let index = 0; index < td_analizar.length; index++) {
+      if ($(this).hasClass("borderrosa")) {
+        text = td_analizar[index].innerHTML;
+        marked = text.replace(valor,"<mark>"+valor+"</mark>");
+        td_analizar[index].innerHTML = marked;
+        
+      } else {
+        
+          td_analizar[index].innerHTML = td_analizar[index].innerHTML.replace(/<\/?mark[^>]*>/g,"");
+          //console.log(valor);
+      }
+      
+    }
+    
+   
+  
   });
 
-
-
-//   var td_analizar = document.querySelectorAll('td');
-//   console.log(td_analizar);
-
-
-
-
-// const buttons = document.querySelectorAll('.resaltar');
-
-// console.log(buttons);
-
-
-// const Resaltar = (e) =>{
-
-//      console.log(e.target.id);
-//     document.getElementById(`${e.target.id}`).classList.add("borderrosa");
-//     // var id_boton = document.getElementById(`${e.target.id}`);
-//     //$('#id_boton').toggleClass("borderrosa borderverde");
-
-    
-       
-//     //  console.log(id_boton);
-// }
-
-
-
-
-
-
-
-
-
-
-
-// //esta funcion me dice que boton click gurdar o editar
-// buttons.forEach((button) => {
-//     //cuando le das click a un boton
-//     button.addEventListener('click', Resaltar);
-    
-// });
+ 
